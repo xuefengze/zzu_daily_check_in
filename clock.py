947a6837ad1df3fa5dab4c68c2c8dcdd27e331dc
xfz <xfz2000x@gmail.com>
2021-11-16 16:34:08 +0800

    update

--- clock.py
+++ clock.py
@@ -2,23 +2,8 @@
 #-*- coding: UTF-8 -*- 
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-import time
-import logging
-import datetime
 import os
 import pandas as pd
-import mail
-
-today_date = str(datetime.date.today())
-log_path = './log/'
-
-logger = logging.getLogger()
-file_handler = logging.FileHandler(filename=log_path+'log_'+today_date+'.txt', mode="a", encoding="utf-8")
-formatter = logging.Formatter('[%(asctime)s]------%(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-
 
 # 开启chrome
 def openChrome():
@@ -27,14 +12,14 @@ def openChrome():
     #options.add_argument('--proxy-server=http://' + proxy_ip)
 
     option = webdriver.ChromeOptions()
-    option.add_argument('disable-infobars')
+    option.add_argument('--disable-infobars')
     option.add_argument('--ignore-certificate-errors')
-    option.add_argument('--headless')
+    #option.add_argument('--headless')
     option.add_argument('--no-sandbox')
     option.add_argument('--disable-gpu')
     option.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(executable_path='./chromedriver', options=option)
-    driver.implicitly_wait(10)  # seconds,隐式等待获取组件，推荐显示，懒得写了
+    driver = webdriver.Chrome(executable_path=os.getcwd()+'/chromedriver', options=option)
+    driver.implicitly_wait(10)  # seconds,隐式等待获取组件，推荐显示
     return driver
 
 def openurl(driver):
@@ -43,13 +28,11 @@ def openurl(driver):
     try:
         driver.get(url)
     except:
-        logging.warning('网址打开失败！')
         return -1
     return driver
 
 # 流程
-def operate_dk(driver, id, password, sender, receiver, smtp):
-    global today_date
+def operate_dk(driver, id, password):
     #input id,password
     driver.find_element_by_xpath("//input[@name='uid']").send_keys(id)
     driver.find_element_by_xpath("//input[@name='upw']").send_keys(password)
@@ -58,10 +41,6 @@ def operate_dk(driver, id, password, sender, receiver, smtp):
     try:
         iframe1 = driver.find_element_by_xpath("//iframe[@name='zzj_top_6s']")
     except:
-        logging.warning(str(id)+'登陆失败！服务器是否又崩了？')
-        mail_title = '每日打卡失败！'
-        mail_body = "打卡失败！登录失败，或服务器崩溃！"
-        mail.sendmessage(sender, receiver, mail_title, mail_body, smtp)
         return -1
     else:
         # 切换frame
@@ -84,6 +63,9 @@ def operate_dk(driver, id, password, sender, receiver, smtp):
         #绿码
         select = Select(driver.find_element_by_name("myvs_13"))
         select.select_by_value("g")
+        #疫苗
+        select = Select(driver.find_element_by_name("myvs_26"))
+        select.select_by_value("2")
 
         #input 具体地址
         ele = driver.find_element_by_xpath("//input[@name='myvs_13c']")
@@ -94,28 +76,15 @@ def operate_dk(driver, id, password, sender, receiver, smtp):
         try:
             driver.find_element_by_css_selector('[onclick="myform52.submit()"]').click()
         except:
-            logging.warning(str(id)+'提交失败！')
-            mail_title = '每日打卡失败！'
-            mail_body = "打卡提交失败！"
-            mail.sendmessage(sender, receiver, mail_title, mail_body, smtp)
             return -1
-        time.sleep(1)
-        logging.warning(str(id)+"打卡成功")
-        mail_title = '每日打卡成功！'
-        mail_body = today_date+"打卡成功！"
-        mail.sendmessage(sender, receiver, mail_title, mail_body, smtp)
 
 
 if __name__ == '__main__':
     driver = openChrome()
-    datas = pd.read_csv('user.csv',dtype=str)
-    sender = 'example@example.com'
-    smtp = mail.openmail(sender)
+    datas = pd.read_csv(os.getcwd()+'/user.csv',dtype=str)
     for row in datas.itertuples():
         id = getattr(row, 'id')
         password = getattr(row, 'password')
-        receiver = getattr(row, 'email')
         driver = openurl(driver)
-        operate_dk(driver, id, password, sender, receiver, smtp)
-    mail.closemail(smtp)
+        operate_dk(driver, id, password)
     driver.quit()
--- readme.md
+++ readme.md
@@ -5,13 +5,12 @@
 * selenium
 * pandas
 * [chromedriver](https://npm.taobao.org/mirrors/chromedriver)
-* email
 
 ### user.csv(example)
 ```
-id,password,email
-1,2,example@example.com
-123456,12345,example@example.com
+id,password
+1,2
+123456,12345
 ```
 
 ### 安装
@@ -19,18 +18,11 @@ id,password,email
 ```
 pip3 install selenium
 ```
-`email`
-```
-pip3 install email
-```
 `chrome`
 ```
 yum install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 ```
-`依赖`
-```
-yum install mesa-libOSMesa-devel gnu-free-sans-fonts wqy-zenhei-fonts
-```
+
 `chromedriver`(换成和chrome对应的版本)
 ```
 wget http://npm.taobao.org/mirrors/chromedriver/92.0.4515.107/chromedriver_linux64.zip
@@ -40,5 +32,3 @@ unzip chromedriver_linux64.zip
 ```
 pip3 install pandas
 ```
-（以及一个垃圾的`log`，仅为了记录是否正常打卡）
-* 使用`cron`时注意路径问题，使用相对路径会报错
